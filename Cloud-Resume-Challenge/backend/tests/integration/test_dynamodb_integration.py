77582b4b9613119ce12367ce3891e7851ad52cec
B0nnie <esn05@yahoo.com>
2024-01-04 18:43:13 -0500

    updated import stmt

--- Cloud-Resume-Challenge/backend/tests/integration/test_dynamodb_integration.py
+++ Cloud-Resume-Challenge/backend/tests/integration/test_dynamodb_integration.py
@@ -2,8 +2,7 @@ import json
 import boto3
 import moto
 import pytest
-#from backend.source.counter import lambda_handler
-from AWS-Cloud-Projects.Cloud-Resume-Challenge.backend.source.counter import lambda_handler
+from ...source.counter import  lambda_handler
 
 @pytest.fixture
 def dynamodb_mock():
